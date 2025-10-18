c7dcb3217a86ec6e0bdfc596eeb487a63be18e5a
Charles.Salameh <charles.salameh@netcetera.com>
2025-10-18 12:45:24 +0400

    add ##

--- envs/test/backend.tf
+++ envs/test/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-charles-demo"     # your S3 bucket created by bootstrap
+    bucket         = "tfstate-charles-demo" # your S3 bucket created by bootstrap
     key            = "envs/test/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"          # enables state locking
+    dynamodb_table = "terraform-locks" # enables state locking
     encrypt        = true
   }
 }
